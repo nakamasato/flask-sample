8679a29484e811d4f75b43ca33d23dd9bd373328
Masato Naka <masatonaka1989@gmail.com>
2024-01-21 07:54:24 +0900

    pre-commit

--- sample/db.py
+++ sample/db.py
@@ -1,24 +1,18 @@
 import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
 
+user = os.getenv("MYSQL_USER", "root")
+password = os.getenv("MYSQL_PASSWORD", "password")
+host = os.getenv("MYSQL_HOST", "localhost")
+port = os.getenv("MYSQL_PORT", "3306")
+database = os.getenv("MYSQL_DATABASE", "test")
 
-user = os.getenv('MYSQL_USER', 'root')
-password = os.getenv('MYSQL_PASSWORD', 'password')
-host = os.getenv('MYSQL_HOST', 'localhost')
-port = os.getenv('MYSQL_PORT', '3306')
-database = os.getenv('MYSQL_DATABASE', 'test')
-
-engine = create_engine(
-    f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
-)
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
 db_session = scoped_session(
-    sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine
-    )
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
 Base = declarative_base()
 Base.query = db_session.query_property()
